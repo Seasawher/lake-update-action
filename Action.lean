c0d7c263d1d6bc33f73c8030486e9dbd12f80e91
Seasawher <tellagoodstory@icloud.com>
2025-12-19 20:33:20 +0900

    明示的に返り値を返す

--- Action.lean
+++ Action.lean
@@ -4,7 +4,7 @@ import Action.Input
 import Action.Output
 
 /-- エントリーポイント -/
-public def main (_ : List String) : IO Unit := do
+public def main (_ : List String) : IO UInt32 := do
   let lakePackageDirectory ← Input.lakePackageDirectory
 
   let toolchainPath : System.FilePath := lakePackageDirectory / "lean-toolchain"
@@ -12,3 +12,5 @@ public def main (_ : List String) : IO Unit := do
   let lakePackageExists ← toolchainPath.pathExists
 
   setOutput "lake_package_exists" (toString lakePackageExists)
+
+  return 0
