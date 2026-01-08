afc5a9e03dd80f36623f0bfd07884b2713604d1b
Seasawher <tellagoodstory@icloud.com>
2026-01-08 23:34:55 +0900

    `public section` で書き直す

--- Action/Input.lean
+++ Action/Input.lean
@@ -4,16 +4,20 @@ open System
 
 namespace Input
 
+public section
+
 /-- 指定された名前の入力を取得する -/
-public protected def get (name : String) : IO String := do
+protected def get (name : String) : IO String := do
   match (← IO.getEnv (s!"INPUT_{name.toUpper}")) with
   | some input => return input
   | none =>
     throw <| IO.userError s!"入力 '{name}' が渡されていません。"
 
 /-- 更新するべきLakeパッケージが存在するディレクトリ。 -/
-public protected def lakePackageDirectory : IO FilePath := do
+protected def lakePackageDirectory : IO FilePath := do
   let dirStr ← Input.get "lake_package_directory"
   return FilePath.mk dirStr
+
+end
 
 end Input
